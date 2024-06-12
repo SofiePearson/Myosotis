512335a5b10db32160a90101ceb7ed9cd6dce561
SofiePearson <81998902+SofiePearson@users.noreply.github.com>
2024-06-12 13:59:10 +1000

    Create Alignment_and_trim.sh

---
+++ 03_Alignment_and_Filtering/Alignment_and_trim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+#SBATCH --account montpt03477
+#SBATCH --job-name Align_Trim_supercontigs
+#SBATCH --cpus-per-task 8
+#SBATCH --time 08:00:00
+#SBATCH --mem 8G
+#SBATCH --output align_trim-%j.out
+#SBATCH --error align_trim-%j.err
+#SBATCH --mail-type ALL
+ 
+module purge
+module load MAFFT/7.429-gimkl-2020a
+module load trimAl/1.4.1-GCC-9.2.0
+ 
+for i in *_supercontig.fasta; do mafft --auto ${i} > ${i%.*}_aln.fasta; done
+ 
+for file in *_aln.fasta; do trimal -in $file -out ${file%%.fasta}_trim.fasta -gt 0.7 -htmlout ${file%%.fasta}_trim.html; done
