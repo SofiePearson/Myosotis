b6453e1c3f90931f6938ee4a882b6f0136dbee1d
SofiePearson <81998902+SofiePearson@users.noreply.github.com>
2024-06-12 13:32:12 +1000

    Update Hybpiper.sh
    Updated hybpiper script to v2

--- 02_Hybpiper/Hybpiper.sh
+++ 02_Hybpiper/Hybpiper.sh
@@ -1,30 +1,6 @@
-#####
-## Install HybPiper
-#####
-
-## In rawsequences folder run:
-git clone https://github.com/mossmatters/HybPiper.git
-cd HybPiper
-
-module purge
-module load Python/3.8.1-gimkl-2018b
-module load SAMtools/1.8-gimkl-2018b
-module load BWA/0.7.17-gimkl-2017a
-module load BLAST/2.9.0-gimkl-2018b
-module load exonerate/2.2.0-gimkl-2017a
-module load SPAdes/3.13.1-gimkl-2018b
-module load Parallel/20200522
-
-## Test to make sure HybPiper will run:
-
-cd /nesi/nobackup/montpt03477/MGS00484/rawsequences/HybPiper/test_dataset
-bash run_tests.sh
-
-
-
-#####
-## Download the file with target sequences containing 6 genera in the Boraginaceae
-#####
+##############################################################
+## Download the mega file with target sequences
+##############################################################
 ## In the New Targets fasta file there are Boraginaceae samples:
 # Ehretia acuminata (EMAL)
 # Heliotropium calcicola (XVRU)
@@ -32,11 +8,9 @@ bash run_tests.sh
 # Mertensia paniculata (DKFZ)
 # Phacelia campanularia (YQIJ)
 # Pholisma arenarium (HANM)
-## These are in the mega353.fasta file. Look at the filtering_options.csv for other species included:
-## https://github.com/chrisjackson-pellicle/NewTargets/blob/master/filtering_options.csv
+## These are in the mega353.fasta file
 
-## In /nesi/nobackup/montpt03477/MGS00484/rawsequences/
-## Clone the repository into the rawsequences folder:
+## Clone the repository into the working folder:
 git clone https://github.com/chrisjackson-pellicle/NewTargets.git
 
 cd /nesi/nobackup/montpt03477/MGS00484/rawsequences/NewTargets
@@ -46,58 +20,51 @@ unzip mega.fasta.zip
 
 ## use mega353 fasta file
 
-
+##############################################################
+## hybpiper_06.sh
+##############################################################
 #!/bin/bash -e
+#SBATCH --job-name=hybpiper8  #job name (shows up in the queue)
+#SBATCH --account=montpt03477
+#SBATCH --cpus-per-task=20   # number of CPUs per task (1 by default)
+#SBATCH --time=60:00:00 #Walltime (HH:MM:SS)
+#SBATCH --mem=60G  #Memory in GB 
 
-#SBATCH --account               montpt03477
-#SBATCH --job-name              HybPiper
-#SBATCH --cpus-per-task         32
-#SBATCH --time                  06:00:00
-#SBATCH --mem                   8G
-#SBATCH --output                HybPiperMiSeq-%j.out
-#SBATCH --error                 HybPiperMiSeq-%j.err
-#SBATCH --mail-type             ALL
-#SBATCH --mail-user             sofie.pearson@hotmail.com
-
-module purge
-module load Python/3.8.1-gimkl-2018b
-module load Miniconda3/4.9.2
+module load Python/3.9.9-gimkl-2020a
+module load HybPiper/2.0.1rc-Miniconda3
 module load Parallel/20200522
-module load SPAdes/3.13.1-gimkl-2018b
-module load exonerate/2.2.0-gimkl-2017a
-module load BLAST/2.9.0-gimkl-2018b
-module load BWA/0.7.17-gimkl-2017a
-module load SAMtools/1.8-gimkl-2018b
 
-# Run HybPiper script
-while read i
-do
-../reads_first.py -r $i*.fq -b mega353.fasta --prefix $i --bwa
-done < namelist.txt
+for file in /nesi/nobackup/montpt03477/Weixuan/angiosperm353/01_trimmedreads/l*_1P.fq
+	do
+	withpath="${file}"
+	filename=${withpath##*/}
+	base="${filename%*_1P.fq}"
+	echo "${base}"
 
-# Get the sequence lengths file
-python ../get_seq_lengths.py mega353.fasta namelist.txt dna > test_seq_lengths.txt
+	hybpiper assemble --run_intronerate \
+	--readfiles /nesi/nobackup/montpt03477/Weixuan/angiosperm353/01_trimmedreads/"${base}"*P.fq \
+	--targetfile_dna mega353.fasta --bwa \
+	--prefix "${base}"_nomerge  --no_padding_supercontigs \
+	--timeout_assemble 600 --paralog_min_length_percentage 0.5
+	done
 
-# Get summary statistics
-python ../hybpiper_stats.py test_seq_lengths.txt namelist.txt > test_stats.txt
+##############################################################
+## hybpiper_exonerator.sh
+##############################################################
+#!/bin/bash -e
+#SBATCH --job-name=hybpiper7  #job name (shows up in the queue)
+#SBATCH --account=montpt03477
+#SBATCH --cpus-per-task=30   # number of CPUs per task (1 by default)
+#SBATCH --time=20:00:00 #Walltime (HH:MM:SS)
+#SBATCH --mem=60G  #Memory in GB
 
-# Test for paralogs
-while read i
-do
-python ../paralog_investigator.py $i
-done < namelist.txt
+module load Python/3.9.9-gimkl-2020a
+module load HybPiper/2.0.1rc-Miniconda3
+module load Parallel/20200522
 
-# Run intronerate
-while read i
-do
-python ../intronerate.py --prefix $i
-done < namelist.txt
 
-#6. run the first 14 samples
+for file in /nesi/nobackup/montpt03477/Weixuan/angiosperm353/01_trimmedreads/*_1P.fq; do         withpath="${file}";         filename=${withpath##*/};         base="${filename%*_1P.fq}";         echo "${base}";          hybpiper assemble --run_intronerate         --readfiles /nesi/nobackup/montpt03477/Weixuan/angiosperm353/01_trimmedreads/"${base}"*P.fq         --targetfile_dna mega353.fasta --bwa         --prefix "${base}"_nomerge  --no_padding_supercontigs         --timeout_assemble 600 --paralog_min_length_percentage 0.5 --start_from exonerate_contigs ; done
 
-sbatch test_myo.sl
-Submitted batch job 23780349
-
-#7. when this has finished, run the cleanup script before going ahead with the remaining 14 samples
-## will need to run the summary and stats scripts after both have finished to get info for all 28 samples (especially for the heatmap)
-
+##############################################################
+## 
+##############################################################
