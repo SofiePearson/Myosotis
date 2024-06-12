945753d1eef92745a1e0755e3440f37f68afe666
SofiePearson <81998902+SofiePearson@users.noreply.github.com>
2024-06-12 14:10:24 +1000

    Create Gene_and_species_trees.sh

---
+++ 04_Trees/Gene_and_species_trees.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+#SBATCH --account montpt03477
+#SBATCH --job-name treebuilding_323_300
+#SBATCH --cpus-per-task 72
+#SBATCH --time 40:00:00
+#SBATCH --mem 72G
+#SBATCH --output treebuilding_323_300-%j.out
+#SBATCH --error treebuilding_323_300-%j.err
+#SBATCH --mail-type ALL
+ 
+module purge
+module load IQ-TREE/2.1.3-gimpi-2020a
+module load Java/15.0.2
+ 
+iqtree2 -S /nesi/nobackup/montpt03477/baits_332/HybPiper2/finaldatasets/323_samples/low_paralogs_300_genes/ -T AUTO -B 1000 --undo
+ 
+./nw_ed low_paralogs_300_genes.treefile 'i & b<=10' o > genetrees_323_samples_300_genes_bs30.treefile
+ 
+java -jar /nesi/nobackup/montpt03477/Astral/astral.5.7.8.jar -i genetrees_323_samples_300_genes_bs10.treefile -o species_tree_from_323_samples_300_genes.treefile --outgroup disc_AT_Vall
