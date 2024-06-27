67a699f52e0ba8252b45645c8cb6c025783706d5
SofiePearson <81998902+SofiePearson@users.noreply.github.com>
2024-06-27 13:03:12 +1000

    Update Gene_and_species_trees.sh
    added example of re-rooting trees

--- 04_Trees/Gene_and_species_trees.sh
+++ 04_Trees/Gene_and_species_trees.sh
@@ -13,7 +13,12 @@ module load IQ-TREE/2.1.3-gimpi-2020a
 module load Java/15.0.2
  
 iqtree2 -S /nesi/nobackup/montpt03477/baits_332/HybPiper2/finaldatasets/323_samples/low_paralogs_300_genes/ -T AUTO -B 1000 --undo
- 
+
 ./nw_ed low_paralogs_300_genes.treefile 'i & b<=10' o > genetrees_323_samples_300_genes_bs30.treefile
- 
+
 java -jar /nesi/nobackup/montpt03477/Astral/astral.5.7.8.jar -i genetrees_323_samples_300_genes_bs10.treefile -o species_tree_from_323_samples_300_genes.treefile --outgroup disc_AT_Vall
+
+#####################################
+## reroot trees at: after subsetting gene trees with outgroup present, after collapsing nodes, and after constructing the species tree.
+## an example of re-rooting the trees after collapsing nodes:
+python reroot_trees.py genetrees_323_samples_300_genes_bs30.treefile > genetrees_323_samples_300_genes_bs30_rerooted.treefile
